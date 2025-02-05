86e7213c5cdccac448bf6f63dc67779a1d3d8180
dwivedishivam17122002 <143676021+dwivedishivam17122002@users.noreply.github.com>
2025-02-05 19:31:00 +0530

    commit

---
+++ stDao/src/stDao/Dao.java
@@ -0,0 +1,12 @@
+package stDao;
+
+public class Dao {
+	  
+	  String driverClassName = "com.mysql.cj.jdbc.Driver";
+	  String connectionUrl = "jdbc:mysql://localhost:3306/st_doa";
+	  String user = "root";
+	  String psw = "Shivam@1976";
+	  
+      private static Dao dao = null;
+    
+}
